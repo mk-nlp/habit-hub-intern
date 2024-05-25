66bf5f72ee254bfeb386db3909a5c8b65407c332
Mert Kısacık <132280028+mk-nlp@users.noreply.github.com>
2024-05-25 22:18:24 +0300

    Update AddTask component state and styles

--- habit-hub/app/tasks/add-task/addTaskState.tsx
+++ habit-hub/app/tasks/add-task/addTaskState.tsx
@@ -3,11 +3,11 @@ import { create } from "zustand";
 // This is just a carry state for the AddTask component
 // Carries the state of the task, category, explanation, task2, and emoji from suggestions
 export const AddTaskStore = create((set) => ({
-  task: "",
-  category: "",
+  task: "Enter a task name",
+  category: "Enter a category",
   explanation: "",
   task2: "",
-  emoji: "",
+  emoji: "⭐",
 
   setTask: (task: string) => set({ task }),
   setCategory: (category: string) => set({ category }),
--- habit-hub/app/tasks/add-task/page.tsx
+++ habit-hub/app/tasks/add-task/page.tsx
@@ -30,17 +30,18 @@ export default function AddTaskPage() {
     Sun: false,
   });
 
+  console.log("NOLUYO LAN:", bgColor);
   const toggleDay = (day) => {
     setSelectedDays({ ...selectedDays, [day]: !selectedDays[day] });
   };
 
   const getButtonColor = (day) => {
-    return selectedDays[day] ? bgColor.slice(0, -3) : "bg-gray-400/60";
+    return selectedDays[day] ? bgColor : "bg-gray-400/60";
   };
   console.log(emoji);
 
   return (
-    <div className={`grid grid-cols-3  ${bgColor}`}>
+    <div className={`grid grid-cols-3  ${bgColor}/60`}>
       <div className="grid col-start-2 justify-center text-8xl mt-8">
         {emoji}
       </div>
@@ -75,31 +76,31 @@ export default function AddTaskPage() {
       <div className="grid grid-cols-7 col-start-1 col-end-4 mt-2 justify-center justify-items-center px-5">
         <Button
           className="w-9 h-9 rounded-full bg-yellow/60 border-2 border-white hover:bg-purple"
-          onClick={() => setBgColor("bg-yellow/60")}
+          onClick={() => setBgColor("bg-yellow")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-green/60 border-2 border-white"
-          onClick={() => setBgColor("bg-green/60")}
+          onClick={() => setBgColor("bg-green")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-purple/60 border-2 border-white"
-          onClick={() => setBgColor("bg-purple/60")}
+          onClick={() => setBgColor("bg-purple")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-pink/60 border-2 border-white"
-          onClick={() => setBgColor("bg-pink/60")}
+          onClick={() => setBgColor("bg-pink")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-red-500/60 border-2 border-white"
-          onClick={() => setBgColor("bg-red-500/60")}
+          onClick={() => setBgColor("bg-red-500")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-lime/60 border-2 border-white"
-          onClick={() => setBgColor("bg-lime/60")}
+          onClick={() => setBgColor("bg-lime")}
         ></Button>
         <Button
           className="w-9 h-9 rounded-full bg-orange/60 border-2 border-white"
-          onClick={() => setBgColor("bg-orange/60")}
+          onClick={() => setBgColor("bg-orange")}
         ></Button>
       </div>
       <div className="grid col-start-1 font-poppins font-bold text-black/80 ml-4 mt-3 ">
@@ -113,9 +114,7 @@ export default function AddTaskPage() {
         <div className="grid row-start-2 grid-cols-3 bg-white font-poppins text-black/60">
           <Button
             className={`grid col-start-1 w-32 ml-11 ${
-              selectedButton === "daily"
-                ? bgColor.slice(0, -3)
-                : unselectedColor
+              selectedButton === "daily" ? bgColor : unselectedColor
             } text-black/60 font-poppins rounded-full`}
             onClick={() => setSelectedButton("daily")}
           >
@@ -123,9 +122,7 @@ export default function AddTaskPage() {
           </Button>
           <Button
             className={`grid col-start-2 w-32 relative z-10 ${
-              selectedButton === "weekly"
-                ? bgColor.slice(0, -3)
-                : unselectedColor
+              selectedButton === "weekly" ? bgColor : unselectedColor
             } text-black/60 font-poppins rounded-full`}
             onClick={() => setSelectedButton("weekly")}
           >
@@ -133,9 +130,7 @@ export default function AddTaskPage() {
           </Button>
           <Button
             className={`grid col-start-3 w-32 -ml-11 ${
-              selectedButton === "monthly"
-                ? bgColor.slice(0, -3)
-                : unselectedColor
+              selectedButton === "monthly" ? bgColor : unselectedColor
             } text-black/60 font-poppins rounded-full`}
             onClick={() => setSelectedButton("monthly")}
           >
@@ -175,29 +170,25 @@ export default function AddTaskPage() {
           </div>
           <div className="grid row-start-2 grid-cols-3 px-2 py-2 bg-white">
             <Button
-              className={`grid col-start-1 w-32 ${
-                selectedRoutine === "daily routine"
-                  ? bgColor.slice(0, -3)
-                  : unselectedColor
+              className={`grid col-start-1 w-32 hover:bg-purple ${
+                selectedRoutine === "daily routine" ? bgColor : unselectedColor
               } text-black/60 font-poppins rounded-full`}
               onClick={() => setSelectedRoutine("daily routine")}
             >
               Daily routine
             </Button>
             <Button
-              className={`grid col-start-2 w-32 relative z-10 ${
-                selectedRoutine === "weekly routine"
-                  ? bgColor.slice(0, -3)
-                  : unselectedColor
+              className={`grid col-start-2 w-32 relative z-10 hover:bg-purple ${
+                selectedRoutine === "weekly routine" ? bgColor : unselectedColor
               } text-black/60 font-poppins rounded-full`}
               onClick={() => setSelectedRoutine("weekly routine")}
             >
               Weekly routine
             </Button>
             <Button
-              className={`grid col-start-3 w-32 ${
+              className={`grid col-start-3 w-32 hover:bg-purple ${
                 selectedRoutine === "monthly routine"
-                  ? bgColor.slice(0, -3)
+                  ? bgColor
                   : unselectedColor
               } text-black/60 font-poppins rounded-full`}
               onClick={() => setSelectedRoutine("monthly routine")}
