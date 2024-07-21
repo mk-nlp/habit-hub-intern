6d5740568e5a42b4d3883bdd7ed2e9946dbe99f8
Mert Kısacık <132280028+mk-nlp@users.noreply.github.com>
2024-07-22 00:42:49 +0300

    chore: Update headers in emptyTasks.tsx

--- habit-hub/components/emptyTasks.tsx
+++ habit-hub/components/emptyTasks.tsx
@@ -22,6 +22,7 @@ export default function EmptyTasks() {
       headers: {
         "Content-Type": "application/json",
         "X-Custom-Origin": process.env.NEXT_PUBLIC_ORIGIN?.toString() ?? "",
+      },
     });
     const data = await response.json();
     for (const task of data) {
